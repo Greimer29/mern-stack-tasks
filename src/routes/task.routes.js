8838bc546992e82bb6042c82ce0ff968bd35bd01
GreimerKmon <greimerabelguzman29@gmail.com>
2021-04-06 22:22:16 -0700

    Create and Read data at database

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -3,13 +3,17 @@ const router = express.Router();
 
 const Task = require('../models/task');
 
-router.get('/', (req, res) => {
-    Task.find((err, tasks) => {
-        console.log(tasks)
-    });
-    res.json({
-        status: "Our API Works and you sucks"
-    });
+router.get('/', async(req, res) => {
+    const tasks = await Task.find();
+    console.log(tasks);
+    res.json(tasks);
+});
+
+router.post('/', async(req, res) => {
+    const { titulo, descripcion } = req.body;
+    const task = new Task({ titulo, descripcion });
+    await task.save();
+    res.json({ status: 'Task saved' });
 });
 
 module.exports = router;
