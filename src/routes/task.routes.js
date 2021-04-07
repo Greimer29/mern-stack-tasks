fd18cd1af2f680095a7ec0f56a133e176af0f6b0
GreimerKmon <greimerabelguzman29@gmail.com>
2021-04-06 18:09:55 -0700

    Using da models and send data to da navigator '#'
    Q
    exit

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const Task = require('../models/task');
+
 router.get('/', (req, res) => {
+    Task.find((err, tasks) => {
+        console.log(tasks)
+    });
     res.json({
-        status: 'API WORS!'
-    })
-})
+        status: "Our API Works and you sucks"
+    });
+});
 
 module.exports = router;
