51873ded6365b78c8b1a813e5c4e4f2f75dc1ca6
GreimerKmon <greimerabelguzman29@gmail.com>
2021-04-06 17:57:37 -0700

    defining da models of our data

---
+++ src/models/task.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const taskSchema = new Schema({
+    titulo: { type: String, required: true },
+    descripcion: { type: String, required: true }
+})
+
+module.exports = mongoose.model('task', taskSchema);
