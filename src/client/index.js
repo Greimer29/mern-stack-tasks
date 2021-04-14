ca48c365579cf7000f59756f8336bc2ee5be7732
GreimerKmon <greimerabelguzman29@gmail.com>
2021-04-14 13:27:59 -0700

    Create a new react component that will converted and sent to the navigator by the new settings

---
+++ src/client/index.js
@@ -0,0 +1,12 @@
+alert('Hello Fucking world');
+
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class App extends Component {
+    render(){
+        return <h1> You are so beatufill little sanada bitch </h1>
+    }
+}
+
+render(<App/> , document.getElementById('app'));
